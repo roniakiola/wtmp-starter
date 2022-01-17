ad3ae9d9c86b4e8902c052f23ced7987457e70d4
Roni Akiola <roniak@metropolia.fi>
2022-01-17 21:00:58 +0200

    task 4

--- src/index.js
+++ src/index.js
@@ -1,18 +1,20 @@
 'use strict';
 
-const coursesEn = ["Hamburger, cream sauce and poiled potates",
-"Goan style fish curry and whole grain rice",
-"Vegan Chili sin carne and whole grain rice",
-"Broccoli puree soup, side salad with two napas",
-"Lunch baguette with BBQ-turkey filling",
-"Cheese / Chicken / Vege / Halloum burger and french fries"];
+import LunchMenu from '../menu.json';
+console.log('lunch menu object', LunchMenu);
 
-const coursesFi = ["Jauhelihapihvi, ruskeaa kermakastiketta ja keitettyä perunaa",
-"Goalaista kalacurrya ja täysjyväriisiä",
-"vegaani Chili sin carne ja täysjyväriisi",
-"Parsakeittoa,lisäkesalaatti kahdella napaksella",
-"Lunch baguette with BBQ-turkey filling",
-"Juusto / Kana / Kasvis / Halloumi burgeri ja ranskalaiset"];
+const coursesFi = [];
+const coursesEn = [];
+
+let arr = Object.entries(LunchMenu.courses);
+console.log(arr);
+
+for (let i = 1; i <= arr.length; i++){
+  coursesFi.push(LunchMenu.courses[i].title_fi);
+  coursesEn.push(LunchMenu.courses[i].title_en);
+}
+
+console.log(coursesFi);
 
 const restaurantCard = document.createElement('div');
 restaurantCard.classList.add('restaurant-card');
