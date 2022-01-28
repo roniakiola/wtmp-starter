15b3fb73693457553f01a3587541096a634d68c3
Roni Akiola <roniak@metropolia.fi>
2022-01-28 23:15:48 +0200

    week3-task1 done

--- src/index.js
+++ src/index.js
@@ -1,75 +1,85 @@
-"use strict";
+'use strict';
 
-import LunchMenu from "./assets/menu.json";
-console.log("lunch menu object", LunchMenu);
+//1:
+// If user types same values as in cheatArray in exact order, will alert be shown on the page
 
-const coursesFi = [];
-const coursesEn = [];
+let cheatArray = ['k', 'a', 'l', 'j', 'a', 'a'];
+//users inputs saved in an array
+let userInserts = [];
+//counter for comparing array values
+let count = 0;
 
-let arr = Object.entries(LunchMenu.courses);
-console.log(arr);
-
-for (let i = 1; i <= arr.length; i++) {
-  coursesFi.push(LunchMenu.courses[i].title_fi);
-  coursesEn.push(LunchMenu.courses[i].title_en);
-}
-
-console.log(coursesFi);
-
-const restaurantCard = document.createElement("div");
-restaurantCard.classList.add("restaurant-card");
-
-const restaurantName = document.createElement("h3");
-restaurantName.textContent = "Ravintola";
-
-const menuContent = document.createElement("p");
-menuContent.textContent = coursesFi;
-
-const languageButton = document.createElement("button");
-languageButton.textContent = "Language: FIN";
-
-const sortButton = document.createElement("button");
-sortButton.textContent = "Sort menu";
-
-const randomDishButton = document.createElement("button");
-randomDishButton.textContent = "Random dish";
-
-restaurantCard.appendChild(restaurantName);
-restaurantCard.appendChild(menuContent);
-restaurantCard.appendChild(languageButton);
-restaurantCard.appendChild(sortButton);
-restaurantCard.appendChild(randomDishButton);
-
-const restaurantContainer = document.querySelector(".restaurant-container");
-restaurantContainer.appendChild(restaurantCard);
-
-sortButton.addEventListener("click", () => {
-  if (menuContent.textContent == coursesFi) {
-    coursesFi.sort();
-    menuContent.textContent = coursesFi;
+window.addEventListener('keyup', (e) => {
+  //comparing pressed key value to array value
+  if (e.key === cheatArray[count]) {
+    userInserts.push(e.key);
+    count++;
   } else {
-    coursesEn.sort();
-    menuContent.textContent = coursesEn;
+    userInserts = [];
+  }
+  if (userInserts.length === cheatArray.length) {
+    alert('Perjantain olut avattu');
+    userInserts = [];
+    count = 0;
   }
 });
+//2:
+//render cursor coodrinates on web page
+const cursorCoordinates = (e) => {
+  const Coords = document.querySelector('p');
+  Coords.innerHTML = 'X:' + e.pageX + '<br>' + 'Y:' + e.pageY;
+};
+//double click triggers the function
+window.addEventListener('dblclick', cursorCoordinates);
 
-languageButton.addEventListener("click", () => {
-  if (menuContent.textContent == coursesFi) {
-    menuContent.textContent = coursesEn;
-    languageButton.textContent = "Language: ENG";
-  } else {
-    menuContent.textContent = coursesFi;
-    languageButton.textContent = "Language: FIN";
-  }
+//3:
+//Mouse over hover element
+const target = document.querySelector('h1');
+
+target.addEventListener('mouseover', (e) => {
+  e.target.innerHTML = 'DONT TOUCH ME >:(';
 });
+target.addEventListener('mouseout', (e) => {
+  e.target.innerHTML = 'Touch me :)';
+});
+//4 & 5:
+//On DOM load: start two timers
+window.addEventListener('DOMContentLoaded', () => {
+  //Timeout works once, then stops
+  setTimeout(() => {
+    const timerText = document.querySelector('.timer-text');
+    timerText.textContent = 'Hurry up!';
+  }, 15000);
 
-randomDishButton.addEventListener("click", () => {
-  if (menuContent.textContent == coursesFi) {
-    var dish = coursesFi[Math.floor(Math.random() * coursesFi.length)];
-  } else {
-    var dish = coursesEn[Math.floor(Math.random() * coursesEn.length)];
-  }
-  const randomDish = document.createElement("p");
-  randomDish.textContent = dish;
-  restaurantCard.appendChild(randomDish);
+  let counter = 0;
+  const idleTimer = document.querySelector('.idle-timer');
+  //every 1 second (1000ms) the counter is ingremented by 1. If mouse movement or keyboard presses are not registered
+  //in 15 seconds, text is then rendered on the page
+  const noMovement = () => {
+    if (counter === 15) {
+      idleTimer.textContent = 'DO SOMETHING!!!';
+      resetTimer();
+    } else {
+      console.log(counter);
+      counter++;
+    }
+  };
+  const resetTimer = () => {
+    counter = 0;
+  };
+
+  setInterval(() => {
+    noMovement();
+  }, 1000);
+
+  //clear textcontent and reset
+  window.onmousemove = () => {
+    idleTimer.textContent = ' ';
+    resetTimer();
+  };
+
+  window.onkeyup = () => {
+    idleTimer.textContent = ' ';
+    resetTimer();
+  };
 });
